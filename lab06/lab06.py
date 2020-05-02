6cefccb2bc7410920992cd04000884b9cf15a348
TATangZY <tangdachan@qq.com>
2020-05-02 16:56:52 +0800

    lab06 finish

--- lab06/lab06.py
+++ lab06/lab06.py
@@ -15,6 +15,14 @@ def make_adder_inc(n):
     11
     """
     "*** YOUR CODE HERE ***"
+    time = -1
+    def adder(num):
+        nonlocal time
+        time += 1
+        return n + num + time
+    
+    return adder
+
 def make_fib():
     """Returns a function that returns the next Fibonacci number
     every time it is called.
@@ -39,6 +47,30 @@ def make_fib():
     True
     """
     "*** YOUR CODE HERE ***"
+    pre1 = 0
+    pre2 = 0
+    time = 0
+    def fib():
+        nonlocal pre1
+        nonlocal pre2
+        nonlocal time
+
+        if time == 0:
+            time += 1
+            return 0
+        if time == 1:
+            time += 1
+            pre1 = 0
+            pre2 = 1
+            return 1
+
+        ret = pre1 + pre2
+        pre1 = pre2
+        pre2 = ret
+
+        return ret
+
+    return fib
 
 # Generators
 def naturals():
@@ -70,6 +102,8 @@ def scale(it, multiplier):
     [2, 4, 6, 8, 10]
     """
     "*** YOUR CODE HERE ***"
+    f = lambda x: multiplier * x
+    yield from map(f, it)
 
 def hailstone(n):
     """
@@ -85,5 +119,12 @@ def hailstone(n):
     1
     """
     "*** YOUR CODE HERE ***"
-
-
+    yield n
+    while n != 1:
+        if n % 2 == 0:
+            n /= 2
+            yield int(n)
+        else:
+            n *= 3
+            n += 1
+            yield int(n)
